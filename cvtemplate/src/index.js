9f87d82b8f29e3f87e79c3cdf4186cbd0616f120
26112000 <accoffshore1@gmail.com>
2024-03-20 20:57:02 +0200

    website

--- cvtemplate/src/index.js
+++ cvtemplate/src/index.js
@@ -1,8 +1,7 @@
 import './scss/styles.scss';
-
 import * as bootstrap from 'bootstrap';
 import particles from "particles.js";
 import "./js/particles.js";
-
 import "../node_modules/typed.js/dist/typed.umd.js";
 import "./js/main.js";
+import "./js/skill.js"
